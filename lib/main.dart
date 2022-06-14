10deee96d989a5e9ca5d007a49ccd51cc391673d
kiran <kiran.kp2004@gmail.com>
2022-06-14 10:53:02 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(Todo());
 }
 
 class Todo extends StatelessWidget {
-  const Todo({ Key? key }) : super(key: key);
+  const Todo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    double d = 24;
+    String Name = "kiran";
+    bool c = true;
+    num n = 35.5;
+    var day = "Tuesday";
+    const pi = 3.14;
     return MaterialApp(
-      home:Material(
-        child:Center(
-        child:Container(
-        child: Text("Welcome Brother !"),
-      ),
-      ),
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome $Name we have $days and $d hours each day!"),
+          ),
+        ),
       ),
     );
   }
