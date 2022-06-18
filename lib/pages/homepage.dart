5b14c8b6a357f54a4bd85101fc91ea6b80c9d009
kiran <kiran.kp2004@gmail.com>
2022-06-19 02:29:56 +0530

    Day 15 completed finally got the expected op

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -7,34 +7,34 @@ import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Kiran";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    final String Name = 'kiran';
-
-    @override
-    loadData() async {
-      await Future.delayed(Duration(seconds: 1));
-      final catalogJson =
-          await rootBundle.loadString("assets/files/catalog.json");
-      final decodedData = jsonDecode(catalogJson);
-      var productsData = decodedData["products"];
-      CatalogModel.items = List.from(productsData)
-          .map<Item>((item) => Item.fromMap(item))
-          .toList();
-      setState(() {});
-      print(productsData);
-    }
-
-    void initState() {
-      super.initState();
-      loadData();
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -45,11 +45,45 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items!.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items![index],
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
                 ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items![index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                        ),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ));
+                },
+                itemCount: CatalogModel.items!.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
